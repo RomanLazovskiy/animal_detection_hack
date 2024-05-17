9c241fdb83d671210444b6c1b96d1a24b0f4811a
Роман Лазовский <r.v.lazovskiy@gmail.com>
2024-05-18 01:29:13 +0500

    Rewrite gui to info, add metadata and reports

--- backend/inference.py
+++ backend/inference.py
@@ -7,19 +7,25 @@ from ultralytics import YOLO
 from PIL import Image, ImageDraw
 import cv2
 import numpy as np
+import pandas as pd
 
 # Загрузка моделей YOLOv8
 detection_model = YOLO('yolov8n.pt')
 classification_model = YOLO('yolov8n-cls.pt')
 
 # Директория для хранения результатов классификации
-history_directory = os.path.join(tempfile.gettempdir(), "classification_history")
-os.makedirs(history_directory, exist_ok=True)
+base_dir = os.path.dirname(os.path.abspath(__file__))
+metadata_directory = os.path.join(base_dir, "metadata")
+reports_directory = os.path.join(base_dir, "reports")
+os.makedirs(metadata_directory, exist_ok=True)
+os.makedirs(reports_directory, exist_ok=True)
+
 
 # Функция для выполнения инференса
 def run_inference(model, img):
     results = model.predict(source=img)
     return results
+
 
 # Функция для обработки изображения для детекции и возвращения bbox и меток
 def process_image_detection(model, image_path):
@@ -39,6 +45,7 @@ def process_image_detection(model, image_path):
 
     return img, detections
 
+
 # Функция для обработки изображений для классификации
 def process_images_classification(model, image_paths):
     class_counts = {}
@@ -55,6 +62,7 @@ def process_images_classification(model, image_paths):
                 image_data["classes"].append(label_name)
         image_classifications.append(image_data)
     return class_counts, image_classifications
+
 
 # Функция для обработки видео для детекции
 def process_video(model, video_path):
@@ -82,6 +90,7 @@ def process_video(model, video_path):
     cap.release()
     cv2.destroyAllWindows()
 
+
 # Функция для обработки архива для классификации
 def process_archive_classification(model, archive_path, extract_to):
     with zipfile.ZipFile(archive_path, 'r') as zip_ref:
@@ -89,11 +98,12 @@ def process_archive_classification(model, archive_path, extract_to):
     image_paths = [os.path.join(extract_to, f) for f in os.listdir(extract_to) if f.endswith(('.png', '.jpg', '.jpeg'))]
     return process_images_classification(model, image_paths)
 
-# Функция для сохранения результатов классификации в JSON файл
+
+# Функция для сохранения результатов классификации в JSON файл и генерации Excel файла
 def save_classification_results(results, image_classifications):
     timestamp = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
     json_filename = f"classification_{timestamp}.json"
-    json_path = os.path.join(history_directory, json_filename)
+    json_path = os.path.join(metadata_directory, json_filename)
     json_data = {
         "timestamp": timestamp,
         "class_counts": results,
@@ -101,19 +111,63 @@ def save_classification_results(results, image_classifications):
     }
     with open(json_path, 'w') as json_file:
         json.dump(json_data, json_file, indent=4)
-    return json_path
+
+    excel_filename = json_filename.replace(".json", ".xlsx")
+    excel_path = os.path.join(reports_directory, excel_filename)
+
+    # Создаем DataFrame из данных JSON
+    rows = []
+    for item in json_data["image_classifications"]:
+        for class_name in item["classes"]:
+            rows.append({"Изображение": item["image"], "Класс": class_name})
+
+    df = pd.DataFrame(rows)
+    df.to_excel(excel_path, index=False)
+
+    return json_path, excel_path
+
 
 # Функция для загрузки всех JSON файлов из директории истории
 def load_history_files():
     history_files = []
-    for filename in os.listdir(history_directory):
+    for filename in os.listdir(metadata_directory):
         if filename.endswith(".json"):
             history_files.append(filename)
     return history_files
 
+
+# Функция для загрузки всех Excel отчетов из директории истории
+def load_report_files():
+    report_files = []
+    for filename in os.listdir(reports_directory):
+        if filename.endswith(".xlsx"):
+            report_files.append(filename)
+    return report_files
+
+
 # Функция для загрузки данных из выбранного JSON файла
 def load_classification_history(filename):
-    json_path = os.path.join(history_directory, filename)
+    json_path = os.path.join(metadata_directory, filename)
     with open(json_path, 'r') as json_file:
         data = json.load(json_file)
     return data
+
+
+# Функция для экспорта данных из JSON файла в Excel
+def export_to_excel(json_filename):
+    json_path = os.path.join(metadata_directory, json_filename)
+    with open(json_path, 'r') as json_file:
+        data = json.load(json_file)
+
+    excel_filename = json_filename.replace(".json", ".xlsx")
+    excel_path = os.path.join(reports_directory, excel_filename)
+
+    # Создаем DataFrame из данных JSON
+    rows = []
+    for item in data["image_classifications"]:
+        for class_name in item["classes"]:
+            rows.append({"Изображение": item["image"], "Класс": class_name})
+
+    df = pd.DataFrame(rows)
+    df.to_excel(excel_path, index=False)
+    return excel_path
--- gui/app.py
+++ gui/app.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import tempfile
 import json
 from PyQt5.QtWidgets import QVBoxLayout, QWidget, QMainWindow, QTabWidget, QPushButton, QFileDialog, QApplication, \
@@ -11,67 +12,94 @@ from io import BytesIO
 
 from backend.inference import process_image_detection, detection_model, process_video, \
     process_archive_classification, classification_model, process_images_classification, save_classification_results, \
-    load_history_files, load_classification_history, history_directory
-import os
+    load_history_files, load_classification_history, export_to_excel, metadata_directory, reports_directory, \
+    load_report_files
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("YOLOv8 Object Detection and Classification")
+        self.setWindowTitle("Классификация парнокопытных")
 
         self.tab_widget = QTabWidget()
         self.setCentralWidget(self.tab_widget)
 
         self.detection_tab = QWidget()
         self.classification_tab = QWidget()
-        self.history_tab = QWidget()
+        self.metadata_tab = QWidget()
+        self.reports_tab = QWidget()
 
-        self.tab_widget.addTab(self.detection_tab, "Detection")
-        self.tab_widget.addTab(self.classification_tab, "Classification")
-        self.tab_widget.addTab(self.history_tab, "History")
+        self.tab_widget.addTab(self.detection_tab, "Детекция")
+        self.tab_widget.addTab(self.classification_tab, "Классификация")
+        self.tab_widget.addTab(self.metadata_tab, "Метаданные")
+        self.tab_widget.addTab(self.reports_tab, "Отчеты")
 
         self.detection_layout = QVBoxLayout()
         self.classification_layout = QVBoxLayout()
-        self.history_layout = QVBoxLayout()
+        self.metadata_layout = QVBoxLayout()
+        self.reports_layout = QVBoxLayout()
 
         self.detection_tab.setLayout(self.detection_layout)
         self.classification_tab.setLayout(self.classification_layout)
-        self.history_tab.setLayout(self.history_layout)
+        self.metadata_tab.setLayout(self.metadata_layout)
+        self.reports_tab.setLayout(self.reports_layout)
 
-        self.detection_button = QPushButton("Load File for Detection")
+        self.detection_button = QPushButton("Загрузить файл для детекции")
         self.detection_button.clicked.connect(self.load_file_detection)
         self.detection_layout.addWidget(self.detection_button)
 
-        self.classification_button = QPushButton("Load File for Classification")
+        self.classification_button = QPushButton("Загрузить файл для классификации")
         self.classification_button.clicked.connect(self.load_file_classification)
         self.classification_layout.addWidget(self.classification_button)
 
-        self.history_list = QListWidget()
-        self.history_layout.addWidget(self.history_list)
+        self.metadata_list = QListWidget()
+        self.metadata_layout.addWidget(self.metadata_list)
 
-        self.view_json_button = QPushButton("View Selected JSON")
+        self.view_json_button = QPushButton("Просмотреть выбранный JSON")
         self.view_json_button.clicked.connect(self.view_selected_json)
-        self.history_layout.addWidget(self.view_json_button)
+        self.metadata_layout.addWidget(self.view_json_button)
 
-        self.view_plot_button = QPushButton("View Selected Classification Plot")
+        self.view_plot_button = QPushButton("Просмотреть график классификации")
         self.view_plot_button.clicked.connect(self.load_selected_classification_history)
-        self.history_layout.addWidget(self.view_plot_button)
+        self.metadata_layout.addWidget(self.view_plot_button)
 
-        self.clear_history_button = QPushButton("Clear History")
-        self.clear_history_button.clicked.connect(self.clear_history)
-        self.history_layout.addWidget(self.clear_history_button)
+        self.export_excel_button = QPushButton("Экспортировать выбранный JSON в Excel")
+        self.export_excel_button.clicked.connect(self.export_selected_to_excel)
+        self.metadata_layout.addWidget(self.export_excel_button)
+
+        self.clear_metadata_button = QPushButton("Очистить метаданные")
+        self.clear_metadata_button.clicked.connect(self.clear_metadata)
+        self.metadata_layout.addWidget(self.clear_metadata_button)
+
+        self.reports_list = QListWidget()
+        self.reports_layout.addWidget(self.reports_list)
+
+        self.open_report_button = QPushButton("Открыть отчет")
+        self.open_report_button.clicked.connect(self.open_report)
+        self.reports_layout.addWidget(self.open_report_button)
+
+        self.clear_reports_button = QPushButton("Очистить отчеты")
+        self.clear_reports_button.clicked.connect(self.clear_reports)
+        self.reports_layout.addWidget(self.clear_reports_button)
 
         self.font_size = 24  # Задаем размер шрифта по умолчанию
 
         self.load_history_files()
+        self.load_report_files()
 
     def load_history_files(self):
-        self.history_list.clear()
+        self.metadata_list.clear()
         history_files = load_history_files()
         for filename in history_files:
             item = QListWidgetItem(filename)
-            self.history_list.addItem(item)
+            self.metadata_list.addItem(item)
+
+    def load_report_files(self):
+        self.reports_list.clear()
+        report_files = load_report_files()
+        for filename in report_files:
+            item = QListWidgetItem(filename)
+            self.reports_list.addItem(item)
 
     def display_plot(self, class_counts):
         buf = BytesIO()
@@ -79,9 +107,9 @@ class MainWindow(QMainWindow):
         labels = list(class_counts.keys())
         counts = list(class_counts.values())
         ax.bar(labels, counts)
-        ax.set_xlabel('Class')
-        ax.set_ylabel('Count')
-        ax.set_title('Classification Results')
+        ax.set_xlabel('Класс')
+        ax.set_ylabel('Количество')
+        ax.set_title('Результаты классификации')
         plt.savefig(buf, format='png')
         buf.seek(0)
 
@@ -94,7 +122,7 @@ class MainWindow(QMainWindow):
         view = QGraphicsView(scene)
 
         dialog = QDialog(self)
-        dialog.setWindowTitle("Classification Results")
+        dialog.setWindowTitle("Результаты классификации")
         dialog.setLayout(QVBoxLayout())
         dialog.layout().addWidget(view)
         dialog.exec_()
@@ -151,14 +179,14 @@ class MainWindow(QMainWindow):
         view.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 
         dialog = QDialog(self)
-        dialog.setWindowTitle("Detection Results")
+        dialog.setWindowTitle("Результаты детекции")
         dialog.setLayout(QVBoxLayout())
         dialog.layout().addWidget(view)
         dialog.exec_()
 
     def load_file_detection(self):
-        file_path, _ = QFileDialog.getOpenFileName(self, "Open File", "",
-                                                   "All Files (*.*);;Image Files (*.png *.jpg *.jpeg);;Video Files (*.mp4 *.avi)")
+        file_path, _ = QFileDialog.getOpenFileName(self, "Открыть файл", "",
+                                                   "Все файлы (*.*);;Изображения (*.png *.jpg *.jpeg);;Видео (*.mp4 *.avi)")
         if not file_path:
             return
 
@@ -168,11 +196,11 @@ class MainWindow(QMainWindow):
         elif file_path.endswith(('.mp4', '.avi')):
             process_video(detection_model, file_path)
         else:
-            self.show_error("Unsupported file format!")
+            self.show_error("Неподдерживаемый формат файла!")
 
     def load_file_classification(self):
-        file_paths, _ = QFileDialog.getOpenFileNames(self, "Open Files", "",
-                                                     "All Files (*.*);;Zip Files (*.zip);;Image Files (*.png *.jpg *.jpeg)")
+        file_paths, _ = QFileDialog.getOpenFileNames(self, "Открыть файлы", "",
+                                                     "Все файлы (*.*);;Архивы (*.zip);;Изображения (*.png *.jpg *.jpeg)")
         if not file_paths:
             return
 
@@ -188,7 +216,7 @@ class MainWindow(QMainWindow):
                     new_class_counts, new_image_classifications = process_images_classification(classification_model,
                                                                                                 [file_path])
                 else:
-                    self.show_error("Unsupported file format for classification!")
+                    self.show_error("Неподдерживаемый формат файла для классификации!")
                     return
                 class_counts.update(new_class_counts)
                 image_classifications.extend(new_image_classifications)
@@ -196,11 +224,12 @@ class MainWindow(QMainWindow):
         save_classification_results(class_counts, image_classifications)
         self.display_plot(class_counts)
         self.load_history_files()  # Обновляем список файлов истории
+        self.load_report_files()  # Обновляем список отчетов
 
     def load_selected_classification_history(self):
-        selected_items = self.history_list.selectedItems()
+        selected_items = self.metadata_list.selectedItems()
         if not selected_items:
-            self.show_error("No history file selected!")
+            self.show_error("Файл истории не выбран!")
             return
 
         filename = selected_items[0].text()
@@ -209,18 +238,18 @@ class MainWindow(QMainWindow):
         self.display_plot(class_counts)
 
     def view_selected_json(self):
-        selected_items = self.history_list.selectedItems()
+        selected_items = self.metadata_list.selectedItems()
         if not selected_items:
-            self.show_error("No history file selected!")
+            self.show_error("Файл истории не выбран!")
             return
 
         filename = selected_items[0].text()
-        json_path = os.path.join(history_directory, filename)
+        json_path = os.path.join(metadata_directory, filename)
         with open(json_path, 'r') as json_file:
             json_data = json.load(json_file)
 
         dialog = QDialog(self)
-        dialog.setWindowTitle("View JSON")
+        dialog.setWindowTitle("Просмотр JSON")
         layout = QVBoxLayout()
         text_edit = QTextEdit()
         text_edit.setPlainText(json.dumps(json_data, indent=4))
@@ -228,18 +257,50 @@ class MainWindow(QMainWindow):
         dialog.setLayout(layout)
         dialog.exec_()
 
-    def clear_history(self):
-        reply = QMessageBox.question(self, 'Clear History', "Are you sure you want to clear all history files?",
+    def export_selected_to_excel(self):
+        selected_items = self.metadata_list.selectedItems()
+        if not selected_items:
+            self.show_error("Файл истории не выбран!")
+            return
+
+        filename = selected_items[0].text()
+        excel_path = export_to_excel(filename)
+        QMessageBox.information(self, "Экспорт успешен", f"Данные экспортированы в Excel файл:\n{excel_path}")
+        self.load_report_files()  # Обновляем список отчетов
+
+    def open_report(self):
+        selected_items = self.reports_list.selectedItems()
+        if not selected_items:
+            self.show_error("Отчет не выбран!")
+            return
+
+        filename = selected_items[0].text()
+        report_path = os.path.join(reports_directory, filename)
+        os.system(
+            f'open "{report_path}"')  # Используем `open` для macOS. Для Windows используйте `start` и для Linux `xdg-open`.
+
+    def clear_metadata(self):
+        reply = QMessageBox.question(self, 'Очистить метаданные', "Вы уверены, что хотите очистить все метаданные?",
                                      QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
         if reply == QMessageBox.Yes:
-            for filename in os.listdir(history_directory):
-                file_path = os.path.join(history_directory, filename)
+            for filename in os.listdir(metadata_directory):
+                file_path = os.path.join(metadata_directory, filename)
                 if os.path.isfile(file_path):
                     os.remove(file_path)
             self.load_history_files()  # Обновляем список файлов истории
 
+    def clear_reports(self):
+        reply = QMessageBox.question(self, 'Очистить отчеты', "Вы уверены, что хотите очистить все отчеты?",
+                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            for filename in os.listdir(reports_directory):
+                file_path = os.path.join(reports_directory, filename)
+                if os.path.isfile(file_path):
+                    os.remove(file_path)
+            self.load_report_files()  # Обновляем список отчетов
+
     def show_error(self, message):
-        QMessageBox.critical(self, "Error", message)
+        QMessageBox.critical(self, "Ошибка", message)
 
 
 if __name__ == "__main__":
